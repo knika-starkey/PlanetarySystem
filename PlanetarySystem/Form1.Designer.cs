8cb913f8625e4eb3d882033347bd4594d2a790b6
knika787 <knika787@gmail.com>
2020-11-01 16:40:59 +0200

    Add project files.

---
+++ PlanetarySystem/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PlanetarySystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbSun = new System.Windows.Forms.PictureBox();
+            this.pbMercury = new System.Windows.Forms.PictureBox();
+            this.pbVenus = new System.Windows.Forms.PictureBox();
+            this.pbEarth = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSun)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMercury)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVenus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEarth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSun
+            // 
+            this.pbSun.Location = new System.Drawing.Point(0, 0);
+            this.pbSun.Name = "pbSun";
+            this.pbSun.Size = new System.Drawing.Size(100, 50);
+            this.pbSun.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSun.TabIndex = 0;
+            this.pbSun.TabStop = false;
+            this.pbSun.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pbMercury
+            // 
+            this.pbMercury.Location = new System.Drawing.Point(165, 12);
+            this.pbMercury.Name = "pbMercury";
+            this.pbMercury.Size = new System.Drawing.Size(100, 50);
+            this.pbMercury.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbMercury.TabIndex = 1;
+            this.pbMercury.TabStop = false;
+            // 
+            // pbVenus
+            // 
+            this.pbVenus.Location = new System.Drawing.Point(21, 64);
+            this.pbVenus.Name = "pbVenus";
+            this.pbVenus.Size = new System.Drawing.Size(100, 50);
+            this.pbVenus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbVenus.TabIndex = 2;
+            this.pbVenus.TabStop = false;
+            // 
+            // pbEarth
+            // 
+            this.pbEarth.Location = new System.Drawing.Point(33, 135);
+            this.pbEarth.Name = "pbEarth";
+            this.pbEarth.Size = new System.Drawing.Size(100, 50);
+            this.pbEarth.TabIndex = 3;
+            this.pbEarth.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(35, 255);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(153, 59);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(139, 150);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(314, 77);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(199, 314);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(376, 181);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pbEarth);
+            this.Controls.Add(this.pbVenus);
+            this.Controls.Add(this.pbMercury);
+            this.Controls.Add(this.pbSun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSun)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMercury)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVenus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEarth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSun;
+        private System.Windows.Forms.PictureBox pbMercury;
+        private System.Windows.Forms.PictureBox pbVenus;
+        private System.Windows.Forms.PictureBox pbEarth;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+    }
+}
+
