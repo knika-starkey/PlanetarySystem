5bef572cc9d46b90e8afecb0cef5e0e4ac58004e
knika787 <knika787@gmail.com>
2020-11-14 16:41:59 +0200

    tried to make the planets move

--- PlanetarySystem/Form1.cs
+++ PlanetarySystem/Form1.cs
@@ -22,8 +22,8 @@ namespace PlanetarySystem
         public Form1()
         {
             InitializeComponent();
-            pbSun.Location = new Point(this.Width / 2 - pbSun.Width / 2, this.Height / 2 - pbSun.Height / 2);
-            pbMercury.Location = new Point(this.Width / 2 - pbMercury.Width / 2 , this.Height / 2 - pbSun.Height / 2- (int)MT.Scale(57910006.0));
+            pbSun.Location = new Point((this.Width - pbSun.Width) / 2, (this.Height - pbSun.Height) / 2);
+            pbMercury.Location = new Point((this.Width - pbMercury.Width) / 2 , this.Height/2 - pbSun.Height/2 - pbMercury.Height- (int)MT.Scale(57910000.0));
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -41,11 +41,11 @@ namespace PlanetarySystem
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            double dt = timer1.Interval / 1000.0 * 300000;
+            double dt = timer1.Interval / 1000.0 * 30000;
 
             Vector m_e = sun.r - mercury.r;
             double r3 = Math.Pow(m_e.Abs, 3);
-            Vector Fe = -6.67e-29 * mercury.m * sun.m * m_e / r3;
+            Vector Fe = -6.67e-29 * sun.m * mercury.m *  m_e / r3 ;
             Vector Fm = -Fe;
 
             mercury.Move(dt, Fm);
